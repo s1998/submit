9891a8bc123fd321f38e270e48d0792ac9de08f4
pihooji@gmail.com <pihooji@gmail.com>
2023-01-24 02:45:16 -0800

    first commit

--- README.md
+++ README.md
@@ -1,3 +1,81 @@
+
+notebooks caontains the cpde used for obtaining the models
+
+Run following commands:
+
+pip install transformers allennlp spacy
+python spacy -m download en_core_web_sm
+
+See the following file : requirementsNew.txt
+
+
+About files in notebooks:
+  notebooks/sailesaichalleng.py
+    This file uses BART in zero-shot fashion to divide sentences into questions and statements.
+    We obtain the files questions.csv and statements.csv .
+    
+    Then the top samples (250 from each file) of the files are labelled manually.
+    This helps in fixing errors + filtering out the filter questions.
+    This finally creates two labelled datasets : questions_lablled.csv and statements_labelled.csv
+
+  notebooks/salesaichallenge2.py
+    This notebook uses the files questions_lablled.csv and statements_labelled.csv to create a model that is
+    used in the final API endpoint and seems to work decently well.
+
+  notebooks/newsalesaichallenge3.py
+    There are two steps here. 
+    First figure out if the sentence mentions a taks and when is it supposed to be done.
+    This can be done using Semnatic Role Labelling.
+    Once we have figured out the sentences that have a verb and a time period, 
+    we use BART in zero-shot fashion to check if the task is to be done after meeting.
+    This creates a file srlTimeV3.csv
+    
+    The top samples from this file are also labelled, which creates srlTimeV3labelled.csv
+
+  notebooks/newsalesaichallenge4.py
+    Using the samples from  srlTimeV3labelled.csv, we create a second classifier that is used for the task of detecting statements that could be potential next steps after the meeting.
+
+
+Models from notebook 2 and 4 are used in the final deployed API.
+
+The second task is significantly harder (to annotate as well as model).
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+Previous documentation of the flask app here : 
+
 # Flask API Hugging Face [facebook/bart-large-mnli](https://huggingface.co/facebook/bart-large-mnli)
 :hugs: Flask API for Hugging Face's `facebook/bart-large-mnli` model
 
--- app.py
+++ app.py
@@ -1,24 +1,171 @@
-from transformers import pipeline
+from transformers import pipeline, AutoTokenizer, AutoModelForSequenceClassification
 from typing import List
 from flask import Flask, request, jsonify
+import re
+import torch
+import numpy as np
+from tqdm.notebook import tqdm
+import os, re
 
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-# create the zero-shot classifier
-classifier = pipeline(
-    "zero-shot-classification", model="facebook/bart-large-mnli"
-)
+questions_model = AutoModelForSequenceClassification.from_pretrained(
+    './questions_model/').to(device)
+new_tokenizer = AutoTokenizer.from_pretrained('bert-base-uncased')
 
+followup_model = AutoModelForSequenceClassification.from_pretrained(
+    './followup_model/').to(device)
+new_tokenizer = AutoTokenizer.from_pretrained('bert-base-uncased')
 
-# classify function
-def classify(sentence: str, labels: List[str], multi_label: int = 0):
-    if (multi_label == 0):
-        return classifier(sentence, labels, multi_label=False)
+def get_prediction(text):
+    encoding = new_tokenizer(text, return_tensors="pt", 
+        padding="max_length", truncation=True, max_length=128)
+    encoding = {k: v.to(questions_model.device) for k,v in encoding.items()}
+
+    outputs = questions_model(**encoding)
+    logits = outputs.logits
+
+    sigmoid = torch.nn.Softmax(dim=-1)
+    probs = sigmoid(logits.squeeze().cpu())
+    probs = probs.detach().numpy()
+    label = np.argmax(probs, axis=-1)
+    
+    if label == 1:
+        return ('question', probs[1])
     else:
-        return classifier(sentence, labels, multi_label=True)
+        return ('statement', probs[0])
 
+def get_prediction_followup(text):
+    encoding = new_tokenizer(text, return_tensors="pt", 
+        padding="max_length", truncation=True, max_length=128)
+    encoding = {k: v.to(questions_model.device) for k,v in encoding.items()}
+
+    outputs = followup_model(**encoding)
+    logits = outputs.logits
+
+    sigmoid = torch.nn.Softmax(dim=-1)
+    probs = sigmoid(logits.squeeze().cpu())
+    probs = probs.detach().numpy()
+    label = np.argmax(probs, axis=-1)
+    
+    if label == 1:
+        return ('followup', probs[1])
+    else:
+        return ('negative', probs[0])
 
 # flask server
 app = Flask(__name__)
+
+import spacy
+nlp = spacy.load('en_core_web_sm') # Load the English Model
+
+
+from allennlp.predictors.predictor import Predictor
+import allennlp_models.tagging
+
+predictor = Predictor.from_path("https://storage.googleapis.com/allennlp-public-models/structured-prediction-srl-bert.2020.12.15.tar.gz",
+                                cuda_device=0)
+
+
+def get_questions(text):
+    print("Here ****** ", text)
+    all_sents = []
+
+    for l in text:
+        l = l.strip()
+        if l.startswith("Client"):
+            l = re.sub("Client [0-9]*:","", l,count=1)
+            doc = nlp(l)
+            for sent in doc.sents:
+                all_sents.append(("client", str(sent)))
+        else:
+            l = " ".join(l.split(":")[1:])
+            doc = nlp(l)
+            for sent in doc.sents:
+                all_sents.append(("host", str(sent)))
+
+    prediction_list = []    
+    for i, (speaker, sent) in enumerate(all_sents):
+        if speaker == "host":
+            continue
+
+        ans = get_prediction(sent)
+        print(sent, ans)
+
+        if ans[0] == "question":
+            prediction_list.append((ans, sent, all_sents[i-5:i+5]))
+
+    return prediction_list
+
+def get_future_work(text, debug=False):
+    
+    all_sents = []
+
+    for l in text:
+        l = l.strip()
+        l = " ".join(l.split(":")[1:])
+        doc = nlp(l)
+        for sent in doc.sents:
+            all_sents.append(str(sent))
+    
+    # included_sents = []
+    # for i, sent in enumerate(all_sents):
+    #     kk = predictor.predict(sentence=sent)
+    #     verbs = kk['verbs']
+
+    #     times = []
+    #     vbs_lst = []
+    #     for verb in verbs:
+    #         include = False
+    #         for t in verb['tags']:
+    #             if 'tmp' in t.lower():
+    #                 include = True
+
+    #         if include:
+    #             times.append([x for x, y in zip(kk['words'], verb['tags']) if 'tmp' in y.lower()])
+    #             vbs_lst.append(verb["verb"])
+
+    # if len(times):
+    #     included_sents.append((times, vbs_lst, sent, all_sents[i-5:i+5]))
+
+    # prediction_list = []    
+    # for i, (times, vbs_lst, sent, context) in enumerate(included_sents):
+    #     ans = get_prediction_followup(sent)
+    #     print(sent, ans)
+
+    #     if ans[0] == "followup":
+    #         prediction_list.append((ans, sent, context))
+
+    prediction_list = []    
+    for i, sent in enumerate(all_sents):
+        ans = get_prediction_followup(sent)
+        if debug:
+            print(sent, ans)
+
+        if ans[0] == "followup":
+            prediction_list.append((ans, sent, all_sents[i-5:i+5]))
+
+    included_sents = []
+    for i, (ans, sent, context) in enumerate(prediction_list):
+        kk = predictor.predict(sentence=sent)
+        verbs = kk['verbs']
+
+        times = []
+        vbs_lst = []
+        for verb in verbs:
+            include = False
+            for t in verb['tags']:
+                if 'tmp' in t.lower():
+                    include = True
+
+            if include:
+                times.append([x for x, y in zip(kk['words'], verb['tags']) if 'tmp' in y.lower()])
+                vbs_lst.append(verb["verb"])
+
+    if len(times) or True:
+        included_sents.append((ans, sent, context))
+
+    return included_sents
 
 # define flash routes
 @app.route("/", methods=["GET", "POST"])
@@ -26,12 +173,39 @@ def index():
     if request.method == "POST":
         data = request.json
 
+
+
         if data is None:
             return jsonify({"error": "Invalid JSON request"})
         elif not ("sentence" in data and "labels" in data):
+            print(data)
+            text = [t.strip() for t in data["text"].split("\n") if t.strip()]
+
+            print("Printing questions ..... ")
+            questions = get_questions(text)
+            questions = sorted(questions, key=lambda x: x[0][1])
+            for ans, question, context in questions:
+                print("\n\t", ans)
+                print("\t", question)
+                for t in context:
+                    print("\t\t", t)
+
+            print("Printing future work ..... ")
+            followup = get_future_work(text, True)
+            followup = sorted(followup, key=lambda x: x[0][1])
+            for ans, task, context in followup:
+                print("\n\t", ans)
+                print("\t", task)
+                for t in context:
+                    print("\t\t", t)
+
+            selected_followup =  [(t[1], t[2]) for t in  followup if t[0][1] > 0.8]
+            if not selected_followup:
+                selected_followup = [(t[1], t[2]) for t in  followup[-3:]]
             return jsonify(
                 {
-                    "error": "'sentence' and/or 'labels' field(s) not present in JSON request"
+                    "question" : [(t[1], t[2]) for t in questions if t[0][1] > 0.8],
+                    "followup" : selected_followup
                 }
             )
         elif not (
